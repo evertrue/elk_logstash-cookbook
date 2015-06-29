490c875905e9f163823a12c5246fa5f0e35aa236
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-29 15:17:48 -0400

    Remove integration tests for the configure recipe

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -8,23 +8,3 @@ describe 'elk_logstash::default' do
     it { should be_enabled }
   end
 end
-
-describe 'elk_logstash::configure' do
-  describe file('/opt/logstash/server/etc/conf.d/input_lumberjack') do
-    it { should be_file }
-    its(:content) { should include 'input' }
-    its(:content) { should include 'lumberjack' }
-  end
-
-  describe file('/opt/logstash/server/etc/conf.d/filter_syslog') do
-    it { should be_file }
-    its(:content) { should include 'filter' }
-    its(:content) { should include 'grok' }
-  end
-
-  describe file('/opt/logstash/server/etc/conf.d/output_elasticsearch') do
-    it { should be_file }
-    its(:content) { should include 'output' }
-    its(:content) { should include 'elasticsearch' }
-  end
-end
